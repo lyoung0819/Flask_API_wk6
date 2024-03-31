1043185492dddb4cff2ce5436d5a77dfeee2271d
lyoung0819 <159396936+lyoung0819@users.noreply.github.com>
2024-03-31 11:44:06 -0500

    progress save

---
+++ app/models.py
@@ -0,0 +1,10 @@
+from . import db
+from datetime import datetime
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String, nullable=False)
+    last_name = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, nullable=False, unique=True)
+    password = db.Column(db.String, nullable=False)
+    date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
