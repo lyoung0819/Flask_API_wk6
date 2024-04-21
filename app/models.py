c5cbef6dc3c893c44977927f838f1fd2b45eaca5
lyoung0819 <159396936+lyoung0819@users.noreply.github.com>
2024-04-21 14:39:29 -0500

    added password hashing to user model

--- app/models.py
+++ app/models.py
@@ -1,13 +1,33 @@
 from . import db
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String, nullable=False)
     last_name = db.Column(db.String, nullable=False)
+    username = db.Column(db.String, nullable=False)
     email = db.Column(db.String, nullable=False, unique=True)
     password = db.Column(db.String, nullable=False)
     date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.set_password(kwargs.get('password', ''))
+
+    def __repr__(self):
+        return f"<User {self.id}|{self.username}>"
+    
+    def set_password(self, plaintext_pass):
+        self.password = generate_password_hash(plaintext_pass)
+        self.save()
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+
+
 
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -15,4 +35,4 @@ class Task(db.Model):
     description = db.Column(db.String, nullable=False)
     completed = db.Column(db.String, nullable=False)
     dueDate = db.Column(db.String, nullable=False)
-    createdAt = db.Column(db.DateTime, nullable=False, default=datetime.utcnow) #if this breaks, look at type in route
+    createdAt = db.Column(db.DateTime, nullable=False, default=datetime.utcnow) 
